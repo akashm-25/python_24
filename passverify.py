dec6f6011426e4826a71f862d34bfc4a2ba48835
akashm-25 <akashaadith@gmail.com>
2020-05-18 11:21:41 +0530

    initaial commit

---
+++ passverify.py
@@ -0,0 +1,5 @@
+year = input('Enter your birth year eg:[1980]')
+if len(year)!=4 or  not year.isdigit() :
+	print("This year is not accepted")
+else:
+	print("input Successful")
---
+++ try.py
@@ -0,0 +1,3 @@
+first_name = 'jacob'
+last_name = 'fulton'
+print(first_name + " " + last_name)
