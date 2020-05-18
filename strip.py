ba13ce2a3066e639bc49d79e0af29ff439b793ce
akashm-25 <akashaadith@gmail.com>
2020-05-18 11:26:36 +0530

    striping python code

---
+++ strip.py
@@ -0,0 +1,5 @@
+#This is the python code to strip of the extra whitespaces at the end
+name = input('enter the name')
+print('initial name length =',len(name))
+name = name.strip()
+print(name,len(name))
